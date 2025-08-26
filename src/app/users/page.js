80a3931715bd26225f942adc7d09432a5f4d3b29
Felipe Valenzuela <feelipevalenzuela@hotmail.com>
2025-08-25 22:15:18 -0300

    mejora users con react

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,8 +1,28 @@
+'use client';
 import UsersList from "./components/UserList";
-import { users } from "../data/mocs";
+//import { users } from "../data/mocs";//
 
+import { useEffect, useState } from "react";
 export default function UsersPage() {
-    //TODO : obtener los usuarios desde un API
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch('https://raw.githubusercontent.com/ORT-PabloFernandez/PNTP2-REACT-EJEMPLO/refs/heads/main/src/data/usersv2.json');
+                const data = await response.json();
+
+                setUsers(data);
+                setLoading(false);
+            }
+            catch (error) {
+                console.error('Error al cargar usuarios', error);
+            }
+        }
+
+        fetchUsers();
+    }, []);
     return (
         <div className="container">
             <UsersList users={users} />
