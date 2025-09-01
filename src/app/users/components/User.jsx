8bd560367fb1a7f7a629e97782a3fbe8009fd3fa
Felipe Valenzuela <feelipevalenzuela@hotmail.com>
2025-09-01 19:29:48 -0300

    fixs

--- src/app/users/components/User.jsx
+++ src/app/users/components/User.jsx
@@ -13,7 +13,7 @@ export default function User({ user }) {
             <div className="user-content">
                 <div className="user-info">
                     <div className="user-avatar">
-                        <img src={`/img/${user.avatar}`} alt={`Foto de ${user.name}`} className="user-image" />
+                        <img src={user.avatar} alt={`Foto de ${user.name}`} className="user-image" />
                     </div>
                     <div className="user-details">
                         <div className="user-name">{user.name}</div>
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 //import { users } from "../data/mocs";
 
 export default function UsersPage() {
-    // TODO: Obtener los usuarios desde un API
+
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -23,7 +23,9 @@ export default function UsersPage() {
                 setLoading(false);
             }
         };
+
         fetchUsers();
+
     }, []);
 
     return (
